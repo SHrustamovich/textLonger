d7ea1674344908e46ea2d952ef794f19c0dc55df
Abdullayev Shohruh <shohruhrustamovich5666@gmail.com>
2021-09-22 23:05:43 +0500

    first commit

---
+++ script/main.js
@@ -0,0 +1,3 @@
+var text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis doloribus sint voluptates est explicabo dicta numquam qui eaque repudiandae id!"
+text.split(' ')
+console.log(text)
